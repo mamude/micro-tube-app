25360982428a7fe6128fd2c740f51ecbcc2cd41d
Samir Mamude <mamude@gmail.com>
2021-10-24 09:14:30 -0300

    refactored

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from .youtube import app_youtube
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(app_youtube)
 
